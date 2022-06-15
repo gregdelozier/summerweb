1adfdd1216951de25b45692003001492205a44cb
Greg <greg@email.com>
2022-06-15 01:10:37 +0000

    added hello route

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def get_index():
-    return "<p>Hello from an extremely interesting web page!!!</p>"
+    return "<p>Hello from an extremely interesting web page!</p>"
 
+@app.route("/hello")
+def get_hello():
+    return "<p>Hello and welcome to Flask!</p>"
