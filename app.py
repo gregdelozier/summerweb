99838a1f8ab320805cec4cc474159f7c514699e4
Greg DeLozier <gregdelozier@gmail.com>
2022-06-21 21:08:30 -0400

    Basic session stuff

--- app.py
+++ app.py
@@ -1,43 +1,48 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect, url_for, make_response
+from flask import session
 
 app = Flask(__name__)
-
-username = None
+app.secret_key="ksmdflkji240[i2hjfsklnf"
 
 @app.route("/")
 @app.route("/home")
 def get_index():
-    username = request.cookies.get("username", None)
-    if username == None:
+    if 'username' in session:
+        username = session['username']
+        custid = session['custid']
+    else:
         return redirect(url_for('get_login'))
     return render_template('home.html', name=username)
 
 @app.route("/other")
 def get_other():
-    username = request.cookies.get("username", None)
-    if username == None:
+    if 'username' in session:
+        username = session['username']
+    else:
         return redirect(url_for('get_login'))
     return render_template('other.html', name=username)
 
 @app.route("/login", methods=['GET'])
 def get_login():
-    username = request.cookies.get("username", None)
-    if username != None:
+    if 'username' in session:
         return redirect(url_for('get_index'))
     return render_template('login.html')
 
 @app.route("/login", methods=['POST'])
 def post_login():
     username = request.form.get("username", None)
-    response = make_response(redirect(url_for('get_index')))
-    response.set_cookie("username", username)
-    return response
+    if username != None:
+        session['username'] = username
+        session['custid'] = 1234
+        return redirect(url_for('get_index'))
+    else:
+        return redirect(url_for('get_login'))
 
 @app.route("/logout", methods=['GET'])
 def get_logout():
-    response = make_response(redirect(url_for('get_login')))
-    response.delete_cookie('username')
-    return response
+    session.pop('username',None) 
+    session.pop('custid',None)
+    return redirect(url_for('get_login'))
 
